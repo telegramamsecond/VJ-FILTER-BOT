e215106bb10935ae3647fc3b7f9f3831aa6aeb61
telegramamsecond <104004811+telegramamsecond@users.noreply.github.com>
2025-02-24 14:43:43 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -75,7 +75,7 @@ async def answer(bot, query):
         results.append(
             InlineQueryResultCachedDocument(
                 title=file['file_name'],
-                document_file_id=file.file_id,
+                document_file_id=file['file_id'],
                 caption=f_caption,
                 description=f'Size: {get_size(file["file_size"])}',
                 reply_markup=reply_markup
